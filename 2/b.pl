0a420f4faf233aa68d0ad5e072e2f93d7462ea25
Kristian Lundström <Mousaka@users.noreply.github.com>
2023-12-03 03:24:54 +0100

    p2 solved. A bitt messy code but can't be bothered to clean it now

---
+++ 2/b.pl
@@ -0,0 +1,69 @@
+:- use_module(library(dcgs)).
+:- use_module(library(pio)).
+:- use_module(library(charsio)).
+
+
+
+number([A|As]) --> [A], { char_type(A, numeric) }, number(As).
+number([])     --> [].
+
+color(blue) --> "blue".
+color(red) --> "red".
+color(green) --> "green".
+
+color_count((N,0,0)) --> number(N0), " ", color(blue), { number_chars(N, N0) }.
+color_count((0,N,0)) --> number(N0), " ", color(red), { number_chars(N, N0) }.
+color_count((0,0,N)) --> number(N0), " ", color(green), { number_chars(N, N0) }.
+
+cube_delimiter --> ", ".
+cube_delimiter, ";" --> ";".
+
+parse_set(Cube) --> color_count(Cube).
+parse_set((B,R,G)) --> 
+  color_count((B1,R1,G1)),
+  cube_delimiter,
+  parse_set((B0,R0,G0)),
+  { B #= B1 + B0,
+    R #= R1 + R0,
+    G #= G1 + G0 }.
+
+
+max(Max, A, B) :-
+  if_( A #< B,
+       Max #= B,
+       Max #= A
+     ).
+
+parse_sets((B,R,G)) --> parse_set((B1,R1,G1)), "; ", parse_sets((B0,R0,G0)),
+                  { max(B, B1, B0),
+                    max(R, R1, R0),
+                    max(G, G1, G0) }. 
+parse_sets(S) --> parse_set(S). 
+
+game_line(Power) --> "Game ", number(_), ": ", parse_sets((B,R,G)),
+       { 
+         Power #= R * G * B
+       }.  
+
+game_id_count([], 0).
+game_id_count([Line|Lines], N) :-
+  phrase(game_line(N1), Line),
+  game_id_count(Lines, N0),
+  N #= N0 + N1.
+
+solve(Sum) :-
+ phrase_from_file(lines(Ls), 'input.txt'), game_id_count(Ls, Sum).
+
+solve_ex(Sum) :-
+ phrase_from_file(lines(Ls), 'example.txt'), game_id_count(Ls, Sum).
+
+% Reading lines
+
+eos([], []).
+
+line([]) -->
+  ( "\n" | call(eos) ).
+line([C|Cs]) --> [C], line(Cs).
+
+lines([]) --> call(eos), !.
+lines([L|Ls]) --> line(L), lines(Ls).
