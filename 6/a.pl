bfd4fb576f613b969e4c1501cd4ddc1e38ce2ceb
Kristian Lundström <Mousaka@users.noreply.github.com>
2023-12-07 08:18:09 +0100

    day 6 part 1

---
+++ 6/a.pl
@@ -0,0 +1,23 @@
+records([(48, 261),(93, 1192),(84,1019),(66,1063)]).
+% records([(7,9), (15,40), (30,200)]).
+
+beats_record(B, (T, D)) :-
+  B #>= 0,
+  TravelT #= T - B,
+  TravelT * B #> D.
+
+product_of_list([], 1).
+product_of_list([H|T], Product) :-
+   product_of_list(T, Rest),
+   Product #= H * Rest.
+
+button_times(All,L):-
+  records(Rs),
+  member((T,D), Rs),
+  findall(B,(beats_record(B,(T,D)), label([B])), All),
+  length(All,L).
+
+solve(S):-
+  findall(L, button_times(_,L), All),
+  product_of_list(All,S).
+ 
