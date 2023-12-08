d84a6e91dbf9da91eec03d82957780e844f9be02
Kristian Lundström <Mousaka@users.noreply.github.com>
2023-12-08 13:12:20 +0100

    fix typo

--- 6/b.pl
+++ 6/b.pl
@@ -17,7 +17,7 @@ solve(S):-
 % An alternative way to solve the problem.
 % Received some feedback that fd_size is relying on implementation
 % choices in the constraint solver and could be unstable to use over time.
-% Got a tip that label([Bottom]) can be used to find the greatest value
+% Got a tip that labeling([down], B), can be used to find the greatest value
 % in the domain in a fast reliable way.
 % Read more https://github.com/mthom/scryer-prolog/discussions/2213#discussioncomment-7794704
 alt_solve(S):-
