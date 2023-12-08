8f0b66c9ae6e06c10bfdae6401f22e371784c404
Kristian Lundström <Mousaka@users.noreply.github.com>
2023-12-08 13:11:29 +0100

    add alternative solver for day6 p 2

--- 6/b.pl
+++ 6/b.pl
@@ -13,4 +13,16 @@ beats_record(B, (T, D)) :-
 solve(S):-
   input(R),
   beats_record(B, R), fd_size(B, S).
+
+% An alternative way to solve the problem.
+% Received some feedback that fd_size is relying on implementation
+% choices in the constraint solver and could be unstable to use over time.
+% Got a tip that label([Bottom]) can be used to find the greatest value
+% in the domain in a fast reliable way.
+% Read more https://github.com/mthom/scryer-prolog/discussions/2213#discussioncomment-7794704
+alt_solve(S):-
+  input(R),
+  once((beats_record(Upper, R), labeling([down],[Upper]))),
+  once((beats_record(Bottom, R), label([Bottom]))),
+  S #= Upper - Bottom + 1.
  
