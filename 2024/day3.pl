5f96c89a7201bc3c0a439205446e2bc2fcda5585
Kristian Lundström <Mousaka@users.noreply.github.com>
2024-12-04 23:20:03 +0100

    temporary giving up on day3 part 2

--- 2024/day3.pl
+++ 2024/day3.pl
@@ -18,3 +18,16 @@ all_muls(N) --> [_], all_muls(N).
 
 solveA(N):-
     phrase_from_file(all_muls(N),'day3_input.txt').
+    
+
+state(S), [S] --> [S].
+state(S0, S), [S] --> [S0].
+
+do(0) --> ( call(eos) ), !.
+do(N) --> "don't()", dont(N).                     %'
+do(N) --> next_mul(N0), {N #= N0 + N1 },do(N1).
+do(N) --> [_], dont(N).
+
+dont(0) --> ( call(eos) ), !.
+dont(N) --> "do()", do(N).
+dont(N) --> [_], dont(N).
