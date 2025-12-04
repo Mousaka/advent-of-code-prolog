1df84d3a88832fcb1fc37dbc4cca3354bfea7328
Kristian Lundström <Mousaka@users.noreply.github.com>
2025-12-04 20:58:05 +0100

    day 1 part 2

--- 2025/day1.pl
+++ 2025/day1.pl
@@ -10,6 +10,7 @@ apply_op(Op, A, B, Result) :-
 l_r(+) --> "R", !.
 l_r(-) --> "L".
 
+
 inner_row(Pos, NextPos, Zeroes) -->
     l_r(Op),
     integer(I),
@@ -32,3 +33,43 @@ row(Pos, Zeroes) -->
 
 solveA(Z) :-
  phrase_from_file(row(50,Z), 'day1_input.txt').
+
+
+ %%%% PART 2
+
+
+extraPass(Pos,V,N):-
+  if_( Pos #= 0
+     , N #= 0
+     , if_((99 #< V; V #< 1 )
+          , N #= 1
+          , N #=0)).
+  
+
+nextPos(Pos, Rot, NextP, Passes) :-
+  NextP #=  (Pos + Rot) mod 100,
+  Passes #= ExtraP + (abs(Rot) // 100),
+  Foo #= Pos + (Rot rem 100),
+  extraPass(Pos,Foo, ExtraP).
+
+
+inner_row2(Pos, NextPos, Zeroes) -->
+    l_r(Op),
+    integer(I),
+    { apply_op(Op, 0, I, Rot),
+      nextPos(Pos, Rot, NextPos, Zeroes)
+    }.
+
+
+row2(Pos, Zeroes) -->
+    inner_row2(Pos, NextPos, Zeroes1),
+    {Zeroes #= Zeroes1 + Zeroes0 },
+    non_zero_ws,
+    row2(NextPos, Zeroes0).
+
+row2(Pos, Zeroes) -->
+  inner_row2(Pos, _, Zeroes),
+  ws, eol.
+
+solveB(Z) :-
+ phrase_from_file(row2(50,Z), 'day1_input.txt').
